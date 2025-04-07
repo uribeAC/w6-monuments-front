1edae3025df21e16345cececc4c534ed2a6a2264
Alex Uribe <repzyyog@gmail.com>
2025-04-07 20:19:41 +0200

    Create Header component with styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="main-header">
+      <h1 className="title">Monuments</h1>
+    </header>
+  );
+};
+
+export default Header;
