0aba17f256f3306c935c3344c2312ac29760e79c
Alex Uribe <repzyyog@gmail.com>
2025-04-07 20:42:40 +0200

    Create Navigation component with styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
+import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 
 const Header: React.FC = () => {
   return (
     <header className="main-header">
       <h1 className="title">Monuments</h1>
+      <Navigation />
     </header>
   );
 };
