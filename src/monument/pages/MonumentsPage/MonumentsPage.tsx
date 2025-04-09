df9a111099591eb675835adcac953e16d9449115
Alex Uribe <repzyyog@gmail.com>
2025-04-09 22:32:09 +0200

    Add MonumentList to MonumentsPage component

--- src/monument/pages/MonumentsPage/MonumentsPage.tsx
+++ src/monument/pages/MonumentsPage/MonumentsPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MonumentList from "../../components/MonumentList/MonumentList";
+import MonumentClient from "../../client/MonumentClient";
+
+const client = new MonumentClient();
+const data = await client.getMonuments();
 
 const MonumentsPage: React.FC = () => {
-  return <></>;
+  return <MonumentList monuments={data} />;
 };
 
 export default MonumentsPage;
