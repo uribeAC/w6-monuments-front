44f07929a759951ba4a1403c64b7217a0316d67e
Alex Uribe <repzyyog@gmail.com>
2025-04-09 13:11:24 +0200

    Delete data folder dead code

--- src/monument/data/monuments.ts
+++
@@ -1,4 +0,0 @@
-import getMonuments from "../client/getMonuments";
-
-const monumentsUrl = "https://monuments-back-uribe.onrender.com/monuments";
-export const monuments = await getMonuments(monumentsUrl);
