91efecb2dc02da84f25e82b4388cbf5f3c728ab7
Alex Uribe <repzyyog@gmail.com>
2025-04-12 10:58:43 +0200

    Add form control on submit

--- src/monument/components/MonumentForm/MonumentForm.tsx
+++ src/monument/components/MonumentForm/MonumentForm.tsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { MonumentData } from "../../types";
 import "./MonumentForm.css";
 
-const MonumentForm: React.FC = () => {
+interface MonumentFormProps {
+  action: () => void;
+}
+
+const MonumentForm: React.FC<MonumentFormProps> = ({ action: action }) => {
+  const initialMonumentData: MonumentData = {
+    name: "",
+    description: "",
+    imageUrl: "",
+    city: "",
+    country: "",
+  };
+
+  const [monumentData, setMonumentData] =
+    useState<MonumentData>(initialMonumentData);
+
+  const changeMonumentData = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const newValue = event.target.value;
+    const propertyName =
+      event.target.id === "image-url" ? "imageUrl" : event.target.id;
+
+    setMonumentData((monumentData) => ({
+      ...monumentData,
+      [propertyName]: newValue,
+    }));
+  };
+
+  const isFormValid =
+    monumentData.name !== "" &&
+    monumentData.description !== "" &&
+    monumentData.imageUrl !== "" &&
+    monumentData.country !== "" &&
+    monumentData.city !== "";
+
+  const onSubmitForm = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    action();
+  };
+
   return (
     <div className="form-wrapper">
       <h2 className="form-title">Introduce monument data:</h2>
-      <form className="monument-form">
+      <form className="monument-form" onSubmit={onSubmitForm}>
         <div className="monument-form__label">
           <label htmlFor="name" className="monument-form__text">
             Name:
@@ -14,6 +57,8 @@ const MonumentForm: React.FC = () => {
             type="text"
             className="monument-form__control"
             id="name"
+            value={monumentData.name}
+            onChange={changeMonumentData}
             required
           />
         </div>
@@ -25,6 +70,8 @@ const MonumentForm: React.FC = () => {
             rows={2}
             className="monument-form__control"
             id="description"
+            value={monumentData.description}
+            onChange={changeMonumentData}
             required
           />
         </div>
@@ -36,6 +83,8 @@ const MonumentForm: React.FC = () => {
             type="url"
             className="monument-form__control"
             id="image-url"
+            value={monumentData.imageUrl}
+            onChange={changeMonumentData}
             required
           />
         </div>
@@ -47,6 +96,8 @@ const MonumentForm: React.FC = () => {
             type="text"
             className="monument-form__control"
             id="country"
+            value={monumentData.country}
+            onChange={changeMonumentData}
             required
           />
         </div>
@@ -58,10 +109,16 @@ const MonumentForm: React.FC = () => {
             type="text"
             className="monument-form__control"
             id="city"
+            value={monumentData.city}
+            onChange={changeMonumentData}
             required
           />
         </div>
-        <button className="monument-form__button" type="submit">
+        <button
+          className="monument-form__button"
+          type="submit"
+          disabled={!isFormValid}
+        >
           Register
         </button>
       </form>
